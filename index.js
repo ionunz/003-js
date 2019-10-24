03b718461f55006e87c93b081dd70288db18d7e3
Jonathan Núñez <estudionunz@gmail.com>
2019-10-24 13:15:39 -0500

    Agrego los ejercicios de Javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+function cambiarFuenteARoboto(){
+    document.querySelector('p').style.fontFamily='Roboto';
+}
+
+function cambiarFuenteAOpenSans(){
+    document.querySelector('p').style.fontFamily='Open Sans';
+}
+
